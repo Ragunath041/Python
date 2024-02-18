7d1d6f7bd01ef702f8d4083fced6420a08439c9f
Ragunath041 <ragunathg00@gmail.com>
2024-02-18 09:21:05 +0530

    pn

--- Programs/82_prime_number_checker.py
+++ Programs/82_prime_number_checker.py
@@ -3,13 +3,20 @@ def is_prime(n):
         return True
     if n <= 1:
         return False
-    if n % 2 == 0:
+    if n % 2 == 0 or n % 3 == 0:
         return False
-    a = int(n ** 0.5) + 1
-    for i in range(3 , a , 2):
+    divisior = int(n ** 0.5) + 1
+    for i in range(3 , divisior , 2):
         if n % i == 0:
             return False
     return True
 
-n = int(input("Enter the Number :"))
-print(is_prime(n))
+def prime_numbers(n):
+    arr = []
+    for i in range(2 , n + 1):
+        if is_prime(i):
+            arr.append(i)
+    return arr
+
+n = int(input('Enter the number :'))
+print(prime_numbers(n))
